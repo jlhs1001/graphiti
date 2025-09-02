52542aed85cd8bff12ae73a72199394841e6d49a
Liam Seewald <jlhs1001@gmail.com>
2025-09-02 03:51:27 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ ".ts", ".js"],
+  },
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "public", "static", "bundle"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  }
+};
